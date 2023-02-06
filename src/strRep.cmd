1b52d653a60b15251a2caf1a63767481c1ba3d69
dbyoung <dbyoung@sina.com>
2023-02-06 16:29:55 +0800

    update strRep.cmd

--- src/strRep.cmd
+++ src/strRep.cmd
@@ -1,13 +1,14 @@
+:: ÎÄ¼þ×Ö·û´®ËÑË÷Ìæ»»
 @echo off
 setlocal EnableDelayedExpansion
  
-set ReplacePath=%1
-set FileType=%2
+set RePath=%1
+set FiType=%2
 set strOld=%3
 set strNew=%4
  
-CD /D %ReplacePath%
+CD /D %RePath%
 
-for /f %%i in ('dir /b /s /a:-d %FileType%') do (
+for /f %%i in ('dir /b /s /a:-d %FiType%') do (
   powershell -Command "(gc %%i) -replace '%strOld%', '%strNew%' | Out-File %%i -encoding UTF8"
 )
