a2d52cee069cf689436b8cbe2dac476cfcc8ddb3
dbyoung <dbyoung@sina.com>
2023-05-29 17:32:02 +0800

    update

--- patch/before_spirv-tools.cmd
+++ patch/before_spirv-tools.cmd
@@ -4,3 +4,4 @@ set SourcePath=%2
 set CodeName=%3
 
 xcopy /e /y /c /i "%DriverPath%\SPIRV-Headers\*.*"   "%SourcePath%\external\SPIRV-Headers\" 
+xcopy /e /y /c /i "%DriverPath%\abseil-cpp\*.*"   "%SourcePath%\external\abseil-cpp\" 
