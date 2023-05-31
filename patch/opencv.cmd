96bb8f1d85161dfadb40ed46b4f624251707580f
dbyoung <dbyoung@sina.com>
2023-05-31 13:23:12 +0800

    update

--- patch/opencv.cmd
+++ patch/opencv.cmd
@@ -25,7 +25,8 @@ if exist %opencvBuildPath% (
 )
 
 :: Брвы
-CMake %Bpara% -DOPENCV_EXTRA_MODULES_PATH=%opencv_contribX% -DOPENCV_ENABLE_NONFREE=ON-DCPU_ONLY=OFF -DWITH_CUDA=ON -DVTK_USE_CUDA=ON -DWITH_JULIA=ON -DHAVE_JULIA=ON -DBUILD_opencv_world=OFF -DCMAKE_INSTALL_PREFIX=%InstallSDKPath% -Thost=%Platform1% -A %Platform2% -B "%opencvBuildPath%" -G "%BuildLanguage%" -S "%SourcePath%"
+CMake %Bpara% ^
+ -DOPENCV_EXTRA_MODULES_PATH=%opencv_contribX% -DOPENCV_ENABLE_NONFREE=ON-DCPU_ONLY=OFF -DWITH_CUDA=ON -DVTK_USE_CUDA=ON -DWITH_JULIA=ON -DHAVE_JULIA=ON -DBUILD_opencv_world=OFF -DCMAKE_INSTALL_PREFIX=%InstallSDKPath% -Thost=%Platform1% -A %Platform2% -B "%opencvBuildPath%" -G "%BuildLanguage%" -S "%SourcePath%"
 CMake %opencvBuildPath%
 Call %BuildRootPath%src\vcm.cmd %BuildRootPath% %opencvBuildPath% %SourceCodeName% %Platform2%
 CMake --build %opencvBuildPath% --config Release --target install
