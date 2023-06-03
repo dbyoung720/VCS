fb9a3eaaef42d8463408fab537886472afd46ccd
dbyoung <dbyoung@sina.com>
2023-06-03 15:38:10 +0800

    update

--- patch/after_leptonica.cmd
+++ patch/after_leptonica.cmd
@@ -5,16 +5,16 @@ set VSSDK=%1
 set "InstallPCPath=%VSSDK:\=/%"
 
 (
-prefix=%InstallPCPath%
-exec_prefix=${prefix}/bin
-libdir=${prefix}/lib
-includedir=${prefix}/include
+echo prefix=%InstallPCPath%
+echo exec_prefix=${prefix}/bin
+echo libdir=${prefix}/lib
+echo includedir=${prefix}/include
 echo.
-Name: leptonica
-Description: An open source C library for efficient image processing and image analysis operations
-Version: 1.84.0
-Requires.private:  zlib libpng libjpeg libtiff-4 libwebp libwebpmux libopenjp2 
-Libs: -L${libdir} -lleptonica-1.84.0 zlib libpng libjpeg libtiff-4 libwebp libwebpmux libopenjp2 zstd giflib liblzma libsharpyuv
-Libs.private:  -lgif -lm
-Cflags: -I${includedir} -I${includedir}/leptonica
+echo Name: leptonica
+echo Description: An open source C library for efficient image processing and image analysis operations
+echo Version: 1.84.0
+echo Requires.private:  zlib libpng libjpeg libtiff-4 libwebp libwebpmux libopenjp2 
+echo Libs: -L${libdir} -lleptonica-1.84.0 zlib libpng libjpeg libtiff-4 libwebp libwebpmux libopenjp2 zstd giflib liblzma libsharpyuv
+echo Libs.private:  -lgif -lm
+echo Cflags: -I${includedir} -I${includedir}/leptonica
 )>%VSSDK%\lib\pkgconfig\%CodeName%.pc
