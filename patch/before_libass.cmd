474f449eee5e0a22caaeb1d22998b1f4717f949a
dbyoung <dbyoung@sina.com>
2023-06-06 16:13:24 +0800

    update

--- patch/before_libass.cmd
+++ patch/before_libass.cmd
@@ -4,4 +4,3 @@ set SourcePath=%2
 set CodeName=%3
 
 xcopy /e /y /c /i "%RootPath%patch\%CodeName%\*.*"   "%SourcePath%\" 
-rem copy /Y "%SourcePath%\libass\ass.h" "%SourcePath%\libass\ass_types.h" 
