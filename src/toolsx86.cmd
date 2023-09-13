897f02abffabe107ecbd8001d7fc54aac687197f
dbyoung <dbyoung@sina.com>
2023-09-13 11:24:05 +0800

    update

--- src/toolsx86.cmd
+++ src/toolsx86.cmd
@@ -1,7 +1,7 @@
 @echo off
 setlocal EnableDelayedExpansion
 
-set "strToolsX86=aria2/7zip/vulkan/Sysinternals/oraclejdk/oraclejre8/ant/bazel/ccache/emscripten/ghostscript/inno-setup/innoextract/innounp/jom/nsis/nginx/python27/python39/postman/scons/ragel/isx/wixtoolset/cuda/octave/ispc/git/sliksvn/mercurial/ruby/pkg-config/perl/cmake/xmake/meson/curl/bison/winflexbison/ninja/swig/nasm/yasm/vswhere/llvm/doxygen/gnuplot/julia/miktex/graphviz/go/rust/rust-msvc/msys2/depends/lazarus/notepad2/notepad3/notepadplusplus/ollydbg/gimp/xnviewmp/mpc-be/vlc/mpv/sqlitespy/heidisql/currports/currprocess/deviceioview/dllexportviewer/driverview/filetypesman/injecteddll/installedcodec/ipnetinfo/myuninstaller/netbscanner/networkconnectlog/networkcounterswatch/networkopenedfiles/networktrafficview/ntfslinksview/openedfilesview/processthreadsview/regdllview/shellexview/shellmenuview/socketsniff/videocacheview/websitesniffer/webvideocap/whatinstartup/wincrashreport"
+set "strToolsX86=aria2/7zip/vulkan/Sysinternals/pandoc/oraclejdk/oraclejre8/ant/bazel/ccache/emscripten/ghostscript/inno-setup/innoextract/innounp/jom/nsis/nginx/python27/python39/postman/scons/ragel/isx/wixtoolset/cuda/octave/ispc/git/sliksvn/mercurial/ruby/pkg-config/perl/cmake/xmake/meson/curl/bison/winflexbison/ninja/swig/nasm/yasm/vswhere/llvm/doxygen/gnuplot/julia/miktex/graphviz/go/rust/rust-msvc/msys2/depends/lazarus/notepad2/notepad3/notepadplusplus/ollydbg/gimp/xnviewmp/mpc-be/vlc/mpv/sqlitespy/heidisql/currports/currprocess/deviceioview/dllexportviewer/driverview/filetypesman/injecteddll/installedcodec/ipnetinfo/myuninstaller/netbscanner/networkconnectlog/networkcounterswatch/networkopenedfiles/networktrafficview/ntfslinksview/openedfilesview/processthreadsview/regdllview/shellexview/shellmenuview/socketsniff/videocacheview/websitesniffer/webvideocap/whatinstartup/wincrashreport"
 :splitx86
 for /f "tokens=1,* delims=/" %%i in ("%strToolsX86%") do (
   set ToolName=%%i
@@ -30,6 +30,8 @@ for /f "tokens=1,* delims=/" %%i in ("%strToolsX86%") do (
     ) else if "%ToolName%" == "postman" (
       call scoop install %ToolName%
     ) else if "%ToolName%" == "Sysinternals" (
+      call scoop install %ToolName%
+    ) else if "%ToolName%" == "pandoc" (
       call scoop install %ToolName%
     ) else (
       call scoop install %ToolName% -a x86
