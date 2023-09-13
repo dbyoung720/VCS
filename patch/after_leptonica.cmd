972d8eeb48492f9e951819214de40a0ffda62a08
dbyoung <dbyoung@sina.com>
2023-09-13 10:25:10 +0800

    update

--- patch/after_leptonica.cmd
+++ patch/after_leptonica.cmd
@@ -14,7 +14,7 @@ echo Name: leptonica
 echo Description: An open source C library for efficient image processing and image analysis operations
 echo Version: 1.84.0
 echo Requires.private:  zlib libpng libjpeg libtiff-4 libwebp libwebpmux libopenjp2 
-echo Libs: -L${libdir} -lleptonica-1.84.0 zlib libpng libjpeg libtiff-4 libwebp libwebpmux libopenjp2 zstd giflib liblzma libsharpyuv
+echo Libs: -L${libdir} -lleptonica-1.84.0 -lzlib -llibpng -ljpeg -ltiff -llibwebp -llibwebpmux -lopenjp2 -lzstd -lgiflib -lliblzma -llibsharpyuv
 echo Libs.private:  -lgif -lm
 echo Cflags: -I${includedir} -I${includedir}/leptonica
 )>%VSSDK%\lib\pkgconfig\%CodeName%.pc
--- patch/after_libxml2.cmd
+++ patch/after_libxml2.cmd
@@ -3,3 +3,18 @@ set VSSDK=%1
 
 copy /Y "%VSSDK%\lib\libxml2s.lib" "%VSSDK%\lib\libxml2.lib"
 
+:: 安装 PC 文件
+set "InstallPCPath=%VSSDK:\=/%"
+
+@echo prefix=%InstallPCPath%>%VSSDK%\lib\pkgconfig\%CodeName%.pc
+@echo libdir=${prefix}/lib>>%VSSDK%\lib\pkgconfig\%CodeName%.pc
+@echo includedir=${prefix}/include>>%VSSDK%\lib\pkgconfig\%CodeName%.pc
+@echo modules=1>>%VSSDK%\lib\pkgconfig\%CodeName%.pc
+@echo.>>%VSSDK%\lib\pkgconfig\%CodeName%.pc
+@echo Name: libXML>>%VSSDK%\lib\pkgconfig\%CodeName%.pc
+@echo Version: 2.12.0>>%VSSDK%\lib\pkgconfig\%CodeName%.pc
+@echo Description: libXML library version2.>>%VSSDK%\lib\pkgconfig\%CodeName%.pc
+@echo Libs: -L${libdir} -lxml2 -lzlib -llzma -liconv -lws2_32 >>%VSSDK%\lib\pkgconfig\%CodeName%.pc
+@echo Libs.private: -lzlib -llzma -liconv -lws2_32 >>%VSSDK%\lib\pkgconfig\%CodeName%.pc 
+@echo Cflags: -I${includedir}/libxml2 -DLIBXML_STATIC>>%VSSDK%\lib\pkgconfig\%CodeName%.pc
+
